98679269961fe1a9f8677ce91d950fcdb56ac6a2
mpaczko <49449218+mpaczko@users.noreply.github.com>
2020-04-16 20:23:14 +0200

    Add files via upload

---
+++ pokemons/src/components/Navbar/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+function Navbar() {
+    return (
+        <div className="Navbar">
+            PokeAPI
+        </div>
+    );
+}
+
+export default Navbar;
